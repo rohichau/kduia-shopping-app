c0308b390f07ea67b8069b48b8a291f70fcd3c35
rohichau <rohitchauhan.iitr@gmal.com>
2023-02-19 02:21:24 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
@@ -8,8 +8,35 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    // <div className="App">
+    //     <CartValue/>
+    //     <Location/>
+    //     <ExpenseList/>
+    //     <ItemSelected/>
+    // </div>
+    <AppProvider>
+        <div className='container'>
+            <h1 className='mt-3'>Shopping App</h1>
+            <div className='row mt-3'>
+                <div className='col-sm'>
+                    <CartValue />
+                </div>
+                <div className='col-sm'>
+                    <Location/>
+                </div>
+            </div>
+            <h2 className='mt-3'>Shopping Cart</h2>
+            <div className='row mt-3'>
+                <div classname='col-lg'>
+                    <ExpenseList/>
+                </div>
+            </div>
+            <h2 className='mt-3'> Add Items</h2>
+            <div className = 'row mt-3'>
+                <ItemSelected/>
+            </div>
+        </div>
+    </AppProvider>
   );
 }
 
